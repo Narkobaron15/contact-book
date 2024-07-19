2eafc0edadd5af4ae1e3cf7ec2badeb8c402929a
Alex Ivanov <oleksii120@gmail.com>
2024-07-19 15:47:27 +0300

    Added routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
+import {ListComponent} from "./list/list.component";
+import {DetailsComponent} from "./details/details.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: ListComponent,
+    pathMatch: 'full'
+  },
+  {
+    path: 'contacts',
+    component: ListComponent,
+  },
+  {
+    path: 'contacts/:id',
+    component: DetailsComponent,
+    // TODO: change to service call
+    title: (route, state) => `${route.paramMap.get('id')}'s info`
+  }
+];
