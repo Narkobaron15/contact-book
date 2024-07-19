4b9192ec77afc8a6534b12bad4d4ee1bd1c9cff4
Alex Ivanov <oleksii120@gmail.com>
2024-07-19 18:55:24 +0300

    Added footer

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
+import {RouterLink} from "@angular/router";
+import {NgOptimizedImage} from "@angular/common";
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterLink,
+    NgOptimizedImage,
+  ],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css'
 })
 export class FooterComponent {
-
+  public currentYear = new Date().getFullYear();
 }
