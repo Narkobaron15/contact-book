fd8f45963206af0a8e41b9267f6ed642e5386060
Alex Ivanov <oleksii120@gmail.com>
2024-07-19 22:22:25 +0300

    Added details page

--- src/app/components/details/details.component.ts
+++ src/app/components/details/details.component.ts
@@ -1,12 +1,59 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import ContactService from "../../services/contact.service";
+import Contact from "../../models/contact";
+import {ActivatedRoute, Router} from "@angular/router";
+import {NgIf, NgOptimizedImage} from "@angular/common";
+import {Title} from "@angular/platform-browser";
+import {SpinnerComponent} from "../spinner/spinner.component";
 
 @Component({
   selector: 'app-details',
   standalone: true,
-  imports: [],
+  imports: [
+    NgIf,
+    NgOptimizedImage,
+    SpinnerComponent,
+  ],
   templateUrl: './details.component.html',
-  styleUrl: './details.component.css'
+  styleUrl: './details.component.css',
 })
-export class DetailsComponent {
+export class DetailsComponent implements OnInit {
+  private _contact?: Contact
 
+  constructor(
+    private service: ContactService,
+    private route: ActivatedRoute,
+    private router: Router,
+    private title: Title,
+  ) {
+  }
+
+  get contact(): Contact {
+    return this._contact!;
+  }
+
+  editContact(): void {
+    this.router.navigate(['/contacts', this.contact.id, 'edit'])
+      .catch(console.error)
+  }
+
+  deleteContact(): void {
+    this.service.deleteContact(this.contact.id)
+    this.router.navigate(['/'])
+      .catch(console.error)
+  }
+
+  ngOnInit(): void {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    const contact = this.service.getContact(id);
+
+    if (!contact) {
+      // redirect to 404
+      this.router.navigate(['/404']).catch(console.error);
+      return
+    }
+
+    this._contact = contact;
+    this.title.setTitle(`${contact.name} details - Contact book`);
+  }
 }
