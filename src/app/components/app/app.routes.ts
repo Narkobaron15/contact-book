9a0259bd33287ea099a2a53fa3f21fa04da71c4a
Alex Ivanov <oleksii120@gmail.com>
2024-07-19 15:55:29 +0300

    Refined project structure

--- src/app/components/app/app.routes.ts
+++ src/app/components/app/app.routes.ts
@@ -1,6 +1,6 @@
 import { Routes } from '@angular/router';
-import {ListComponent} from "./list/list.component";
-import {DetailsComponent} from "./details/details.component";
+import {ListComponent} from "../list/list.component";
+import {DetailsComponent} from "../details/details.component";
 
 export const routes: Routes = [
   {
