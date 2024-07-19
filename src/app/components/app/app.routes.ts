f9682966d191d90314638dd32416267196affee7
Alex Ivanov <oleksii120@gmail.com>
2024-07-19 16:02:24 +0300

    Started adding not found page

--- src/app/components/app/app.routes.ts
+++ src/app/components/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import {ListComponent} from "../list/list.component";
 import {DetailsComponent} from "../details/details.component";
+import {NotFoundComponent} from "../not-found/not-found.component";
 
 export const routes: Routes = [
   {
@@ -10,12 +11,18 @@ export const routes: Routes = [
   },
   {
     path: 'contacts',
+    pathMatch: 'full',
     component: ListComponent,
   },
   {
     path: 'contacts/:id',
     component: DetailsComponent,
+    pathMatch: 'full',
     // TODO: change to service call
     title: (route, _state) => `${route.paramMap.get('id')}'s info`
+  },
+  {
+    path: '**',
+    component: NotFoundComponent,
   }
 ];
